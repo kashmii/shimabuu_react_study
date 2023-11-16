7f4f4cd5afff77041cd74124c1e6ac40045b9220
kashmii <www.myac@gmail.com>
2023-11-16 14:44:58 +0900

    ✨ headerの追加

---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import classes from './header.module.css'
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <div className={classes.anchor}>Index</div>
+      </Link>
+      <Link href="/about">
+        <div className={classes.anchor}>About</div>
+      </Link>
+    </header>
+  )
+}
