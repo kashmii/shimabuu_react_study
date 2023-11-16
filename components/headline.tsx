abc771a2dd6cd24fe1c3bc4a3c01cbcd300d62ff
kashmii <www.myac@gmail.com>
2023-11-16 10:34:21 +0900

    ♻️ページタイトル部分をコンポーネント化

---
+++ components/headline.tsx
@@ -0,0 +1,9 @@
+export function Headline(props: { page_name: string }) {
+  console.log(props.page_name)
+  return (
+    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      Get started by editing&nbsp;
+      <code className="font-mono font-bold">pages/{props.page_name}.tsx</code>
+    </p>
+  )
+}
